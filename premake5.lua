04c4d7112f56e605e38a09f9e09aca063224f879
JJJJJJJustin <2485400194@qq.com>
2024-04-07 21:22:49 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -17,14 +18,14 @@ project "GLFW"
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
-        "src/platform.c",       --
+        "src/platform.c",           --
         "src/vulkan.c",
         "src/window.c",
         "src/egl_context.c",        --
         "src/osmesa_context.c",     --
         "src/null_platform.h",      --
         "src/null_joystick.h",      --
-        "src/null_init.c",      --
+        "src/null_init.c",          --
     
         "src/null_monitor.c",       --
         "src/null_window.c",        --
@@ -32,9 +33,7 @@ project "GLFW"
     }
 
 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
-        staticruntime "On"
 
         files
         {
@@ -57,5 +56,12 @@ project "GLFW"
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
         }
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+        
